e0a579678a35bf67b0fe92c54c236049dfe2c1f7
Shubham Biswal <subhambiswal.1319@gmail.com>
2026-06-17 22:01:28 +0530

    bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 export const io = new Server(server,{
     cors:{
-        origin: "https://insync-taupe.vercel.app",
+        origin: "https://insync-taupe.vercel.app/",
         credentials:true
     }
 })
